d5369e19bed2031be243a2f3d275f19589be64bf
ThanhTung19431261 <220455853+Wltung@users.noreply.github.com>
2026-03-19 14:54:37 +0700

    Update next config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        // Khi trình duyệt gọi /api/...
+        source: '/api/:path*',
+        // Vercel sẽ ngầm chuyển tiếp đến Render
+        destination: 'https://rewind-api-2muu.onrender.com/api/:path*', 
+      },
+    ]
+  },
 };
 
 export default nextConfig;
